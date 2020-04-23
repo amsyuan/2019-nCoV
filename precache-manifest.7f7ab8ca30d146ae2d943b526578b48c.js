647f5cccdd47e267d4ca9ad98163cd2c35e9cafa
amsyuan <yuan.z.jin@gmail.com>
2020-04-23 09:23:27 +0800

    Updates

---
+++ precache-manifest.7f7ab8ca30d146ae2d943b526578b48c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db39175944b56ba72c1d8d6b3ea3ef6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "507ab1c5ca7804650a93",
+    "url": "./static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "95239c74fb44b4f14314",
+    "url": "./static/css/main.b20f4b86.chunk.css"
+  },
+  {
+    "revision": "507ab1c5ca7804650a93",
+    "url": "./static/js/2.59c1b5e0.chunk.js"
+  },
+  {
+    "revision": "b9a30bc558be0f775546588c9752fe69",
+    "url": "./static/js/2.59c1b5e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95239c74fb44b4f14314",
+    "url": "./static/js/main.0666470c.chunk.js"
+  },
+  {
+    "revision": "4c99a0e1b2171c23871d",
+    "url": "./static/js/runtime-main.393d7ca8.js"
+  },
+  {
+    "revision": "487fdc083f77ed81c35581dfc03e8bb9",
+    "url": "./static/media/feiyan.487fdc08.jpeg"
+  }
+]);
